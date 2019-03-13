df3441ade9042151e4071b3ff293cd96093e3274
Snh23 <47568328+Snh23@users.noreply.github.com>
2019-03-13 16:01:56 -0600

    1

---
+++ wikimeypar/src/Xalva/Pag01.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+class Pag01 extends Component {
+constructor () {
+  super();
+}
+render(){
+    return(
+<div height = "90%">
+<table cellspacing="10" cellpadding="10" border="0" > 
+<tr> 
+  <td>
+    Principal
+  </td>
+       <td align="center"> 
+        <table cellspacing="2" cellpadding="2" border="0"> 
+        <tr> 
+            <td>
+            secundaria 1
+           </td> 
+            <td>
+            </td> 
+        </tr> 
+        <tr>
+          <td>
+        secundaria 2
+        </td>
+        </tr>
+        </table> 
+    </td> 
+</tr> 
+</table>
+
+</div>
+    )
+}
+}
+export default Pag01
+
