41eefbfacacc68993ff2718fe68f2fb5c857b333
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-03-15 17:01:50 -0600

    Create Landing.js

---
+++ wikimeypar/src/Components/Landing.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Landing extends Component{
+   constructor(){
+       super();
+   }
+   render(){
+       return(
+           <div>
+               <h1>Hola morros</h1>
+           </div>
+       )
+   }
+}
+
+export default Landing;
