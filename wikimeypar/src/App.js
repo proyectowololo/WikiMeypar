f4f9824ba3519e2e5bbcd207cde02ad63999ceaf
Snh23 <47568328+Snh23@users.noreply.github.com>
2019-03-15 16:56:53 -0600

    6

--- wikimeypar/src/App.js
+++ wikimeypar/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './aj0c06i.gif';
 import './App.css';
-import LeerLogs from './snh/LeerLogs';
-import ADV_PS_Efectivo from './snh/ADV_PS_Efectivo';
 
 class App extends Component {
   render() {
@@ -11,8 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <LeerLogs/>
-            <ADV_PS_Efectivo/>
+            
           </p>
                   
         </header>
