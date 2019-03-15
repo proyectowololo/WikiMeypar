b9b2e45f5a59db3fa92aba230f3a858acdb0c266
Snh23 <47568328+Snh23@users.noreply.github.com>
2019-03-15 16:14:37 -0600

    Update App.js

--- wikimeypar/src/App.js
+++ wikimeypar/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './aj0c06i.gif';
 import './App.css';
+import LeerLogs from './snh/LeerLogs';
+import ADV_PS_Efectivo from './snh/ADV_PS_Efectivo';
 
 class App extends Component {
   render() {
@@ -9,9 +11,10 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Bienvenidos a Wololo Meypar
+            <LeerLogs/>
+            <ADV_PS_Efectivo/>
           </p>
-          
+                  
         </header>
       </div>
     );
