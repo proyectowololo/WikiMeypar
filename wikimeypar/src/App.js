e44ca87a5c673ab642d5924cfbe1f6a7f52c1ac7
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-03-19 17:24:59 -0600

    Update App.js

--- wikimeypar/src/App.js
+++ wikimeypar/src/App.js
@@ -1,20 +1,39 @@
+/*IMPORTAR DEPENDENCIAS*/
 import React, { Component } from 'react';
-import logo from './aj0c06i.gif';
-import './App.css';
+import ReactBootstrapCarousel from "react-bootstrap-carousel";
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+
+/*IMPORTAR CSS*/
+import '../src/css/style.css'
+import '../src/css/animate.css'
+import '../src/css/bootstrap.css'
+import '../src/css/flexslider.css'
+import '../src/css/icomoon.css'
+import '../src/css/magnific-popup.css'
+import '../src/css/owl.carousel.min.css'
+import '../src/css/owl.theme.default.min.css'
+
+/*IMPORTAR COMPONENTES*/
 import LeerLogs from './snh/LeerLogs';
 import ADV_PS_Efectivo from './snh/ADV_PS_Efectivo';
 import Landing from './Components/Landing'
+import Nav from './Components/Nav'
+import Presentation from './Components/Landing/Presentation'
+import Carousel from './Components/Landing/Carousel'
+import Information from './Components/Landing/Information'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <Landing/>
-            <LeerLogs/>
-            <ADV_PS_Efectivo/>
+            <Nav/>
+            <Presentation/>
+            <Carousel/>
+            <Information/>
           </p>
                   
         </header>
