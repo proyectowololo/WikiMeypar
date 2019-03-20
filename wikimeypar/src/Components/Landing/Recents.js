20d6114ad784904ff17d0a7cd54023fb885788f8
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-03-20 16:11:04 -0600

    Create Recents.js

---
+++ wikimeypar/src/Components/Landing/Recents.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Recents extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Recents;
