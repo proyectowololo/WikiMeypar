0efd794486edfc3b18e2988c74c93cb24c0f6f43
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-03-20 17:28:42 -0600

    Update Router.js

--- wikimeypar/src/Router.js
+++ wikimeypar/src/Router.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {Switch, Route} from 'react-router-dom'
 import Landing from './Components/Landing'
 import Incidencia from './adan/IncidenciasForm'
+import Login from './adan/LoginForm'
+import Registro from './adan/RegistroForm'
 
 const Router = () => (
     <Switch>
     <Route exact path="/" component={Landing} />
     <Route exact path="/incidencia" component={Incidencia} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/registro" component={Registro} />
     </Switch>
 )
 
