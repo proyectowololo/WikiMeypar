248aec86c99888cc0454b27725321e6f95d3ceb8
gzuppa <40968747+gzuppa@users.noreply.github.com>
2019-03-28 17:03:01 -0600

    manuales

--- wikimeypar/Front/src/Router.js
+++ wikimeypar/Front/src/Router.js
@@ -5,9 +5,8 @@ import Incidencia from './adan/IncidenciasForm'
 import Login from './adan/LoginForm'
 import Registro from './adan/RegistroForm'
 import Boleta from './adan/BoletaServicioFrom'
-import Log from './snh/ADV_PS_Efectivo'
-import Manuales from './adan/Manuales'
 import Admin from './Components/Admin'
+import MainManuales from './Components/Manuales/MainManuales'
 
 const Router = () => (
 
@@ -17,8 +16,8 @@ const Router = () => (
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Registro} />
         <Route exact path="/boleta" component={Boleta} />
-        <Route exact path="/log" component={Log} />
         <Route exact path="/main/admin" component={Admin} />
+        <Route exact path="/manualeslist" component={MainManuales} />
     </Switch>
 )
 
